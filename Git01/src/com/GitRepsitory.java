b92405b03e59de9972739cf5aec17c4fe55bdb4f
ASUS <ASUS@LAPTOP-RN565PT4>
2022-08-02 10:40:03 +0530

    By Mahesh

--- Git01/src/com/GitRepsitory.java
+++ Git01/src/com/GitRepsitory.java
@@ -10,9 +10,9 @@ public class GitRepsitory {
 	{
 	  System.out.println("This is M2");
 	}
-	public void m2()
+	public void m3()
 	{
-	  System.out.println("This is M2");
+	  System.out.println("This is M3");
 	}
 
 	public static void main(String[] args) {
